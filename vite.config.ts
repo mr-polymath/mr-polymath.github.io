b88e58fd888e292aa9524666ee550d25e550def0
konstantinos <no@mail.yet>
2025-08-01 10:51:06 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: "/konstantinos-portfolio",
+  base: "/",
 })
